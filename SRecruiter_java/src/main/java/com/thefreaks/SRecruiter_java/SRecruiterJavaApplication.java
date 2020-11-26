7e2addf668d1c8b12dc9af41a7c554efcfdae7f1
Arjanit <ak35372@universum-ks.org>
2020-11-26 14:13:58 +0100

    Initial commit

---
+++ SRecruiter_java/src/main/java/com/thefreaks/SRecruiter_java/SRecruiterJavaApplication.java
@@ -0,0 +1,13 @@
+package com.thefreaks.SRecruiter_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SRecruiterJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SRecruiterJavaApplication.class, args);
+	}
+
+}
