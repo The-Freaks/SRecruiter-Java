08ba64f1ef5dc7faeb8f2f656f6d7cd59c12fec9
Arjanit <ak35372@universum-ks.org>
2020-12-02 17:54:49 +0100

    Finished the Login and Registration functions

--- SRecruiter_java/src/main/java/com/thefreaks/SRecruiter_java/Controller/MainController.java
+++ SRecruiter_java/src/main/java/com/thefreaks/SRecruiter_java/Controller/MainController.java
@@ -1,5 +1,8 @@
 package com.thefreaks.SRecruiter_java.Controller;
 
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,6 +17,18 @@ public class MainController {
 
     @GetMapping("/login")
     public String login(Model model) {
+        if (isAuthenticated()) {
+            return "redirect:/";
+        }
         return "login";
+    }
+
+    private boolean isAuthenticated() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication == null || AnonymousAuthenticationToken.class.
+          isAssignableFrom(authentication.getClass())) {
+            return false;
+        }
+        return authentication.isAuthenticated();
     }
 }
